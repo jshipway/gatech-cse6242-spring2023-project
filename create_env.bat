bbce930244e07439c65ed407c2321a368a2f0c99
stringofQs <100437339+stringofQs@users.noreply.github.com>
2023-03-12 13:55:55 -0400

    Modified env creation batch script
    Modified environment creation batch script with more relevant packages

--- create_env.bat
+++ create_env.bat
@@ -2,5 +2,7 @@ CALL conda create --name flt_rsk python=3.10
 CALL activate flt_rsk
 CALL conda install pandas=1.5
 CALL conda install scikit-learn
+CALL conda install openpyxl
+CALL pip install pymap3d
 CALL pip install --editable=utils
 pause

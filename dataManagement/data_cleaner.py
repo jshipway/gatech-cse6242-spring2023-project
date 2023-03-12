f48b87143b9ec65f8d810d20852c06b5fd4a7734
stringofQs <100437339+stringofQs@users.noreply.github.com>
2023-03-12 13:56:37 -0400

    Data cleaning improvements
    More data cleaning improvements and creation of supplemental data for assembling data sets

--- dataManagement/data_cleaner.py
+++ dataManagement/data_cleaner.py
@@ -2,8 +2,10 @@
 import pandas as pd
 from pathlib import Path
 import zipfile
+from utils import coordinate_utils as cu
 
-_ZIP_DATA_DIR_ = r'C:\Users\pfarmer8-gtri\Box\Paul_Farmer\Grad School\Material\CSE6242 - Data and Visual Analytics\Project\Marketing On-time Data 2018-19'
+
+_ZIP_DATA_DIR_ = r'C:\Users\Paul\OneDrive - Georgia Institute of Technology\Grad_School\Courses\CSE6242\Projects\dataset\Marketing On-time Data 2018-19'
 _CSV_NAME_BASE = 'On_Time_Marketing_Carrier_On_Time_Performance_(Beginning_January_2018)_'
 _COLS_OF_INTEREST = ['Year', 'Quarter', 'Month', 'DayofMonth', 'DayOfWeek',
                      'Marketing_Airline_Network', 'Operated_or_Branded_Code_Share_Partners',
@@ -17,7 +19,7 @@ _COLS_OF_INTEREST = ['Year', 'Quarter', 'Month', 'DayofMonth', 'DayOfWeek',
 def extract_csv_from_zip(zip_file_path):
     if Path(zip_file_path).exists():
         data_month = str(zip_file_path).split('_Performance_')[1].replace('.zip', '')
-        extract_dir = Path(Path(zip_file_path).parent, 'data_extracted')
+        extract_dir = Path(Path(zip_file_path).parent, 'temp_extracted')
         if not extract_dir.exists():
             extract_dir.mkdir(parents=True)
 
@@ -47,18 +49,82 @@ def clean_dataframe(df, prime_airport=None):
         if prime_airport is not None:
             print('Could not find the defined airport in the dataset: {}'.format(prime_airport))
         final_df = df.copy()
+    all_unq_airports = list(set(list(df['Origin'].unique()) + list(df['Dest'].unique())))
+    busy_count = {'Airport': ['Dep', 'Arr']}
+    for apt in all_unq_airports:
+        if apt not in busy_count:
+            busy_count[apt] = [0, 0]
+        busy_count[apt][0] = (df.Origin.values == apt).sum()
+        busy_count[apt][1] = (df.Dest.values == apt).sum()
 
-    fixed_df = finalize_dataset(final_df)
+    airport_size_df = pd.DataFrame(busy_count).reset_index(drop=True)
+    airport_size_df.to_csv('supplemental_data/airport_size.csv')
+    fixed_df = finalize_dataset(final_df, airport_size_df)
 
     return fixed_df
 
 
-def finalize_dataset(df):
+def finalize_dataset(df, airport_size_df):
     # This method is intended to finalize all the features needed for the ML process, as well as
     # any other columns needed for visualization
+    airport_locs = pd.ExcelFile('supplemental_data/Airport_locations_v2.xlsx')\
+        .parse(sheet_name='Airports in Aug2022 On tim Data')
+    hubs = pd.read_excel('supplemental_data/Hubs.xlsx', skiprows=0, header=1)
+    sub_df = df.copy()
+    sub_df = sub_df.merge(airport_locs, left_on='Origin', right_on='Airport')
+    sub_df = sub_df.rename(mapper={'FAA_Lat': 'Origin_Lat', 'FAA_Long': 'Origin_Lon', 'ST_zone_adj': 'Origin_TZ_Adjust'}, axis=1, errors='raise')
+    sub_df.drop(['Airport', 'Airport_city', 'Airport_State', 'Time_Zone', 'TZ_adjust', 'TimeZone_nm'],
+                inplace=True, axis=1)
+    sub_df = sub_df.merge(airport_locs, left_on='Dest', right_on='Airport')
+    sub_df = sub_df.rename(mapper={'FAA_Lat': 'Dest_Lat', 'FAA_Long': 'Dest_Lon', 'ST_zone_adj': 'Dest_TZ_Adjust'}, axis=1, errors='raise')
+    sub_df.drop(['Airport', 'Airport_city', 'Airport_State', 'Time_Zone', 'TZ_adjust', 'TimeZone_nm'],
+                inplace=True, axis=1)
+    unnamed_cols = [i for i in sub_df.columns if 'unnamed' in i.lower()]
+    sub_df.drop(unnamed_cols, inplace=True, axis=1)
 
-    # TODO: Actually make this do something lol
-    return df.copy()
+    dist = []
+    dirctn = []
+    ap_size = {'orgn': [], 'dest': []}
+    hub_stat = {'orgn': [], 'dest': []}
+    for ndx, row in sub_df.iterrows():
+        # Get hub status
+        orgn = getattr(row, 'Origin')
+        dest = getattr(row, 'Dest')
+        carrier = getattr(row, 'Marketing_Airline_Network')
+        if (carrier in hubs.columns) and (orgn in list(hubs['Airport'].unique())):
+            hub_stat['orgn'].append(hubs[hubs['Airport'] == orgn][carrier].iloc[0])
+        else:
+            hub_stat['orgn'].append(0)  # If not present, is not a hub
+
+        if (carrier in hubs.columns) and (dest in list(hubs['Airport'].unique())):
+            hub_stat['dest'].append(hubs[hubs['Airport'] == dest][carrier].iloc[0])
+        else:
+            hub_stat['dest'].append(0)  # If not present, is not a hub
+
+        if orgn in airport_size_df.columns:
+            ap_size['orgn'].append(airport_size_df[orgn].iloc[0] + airport_size_df[orgn].iloc[1])
+        else:
+            ap_size['orgn'].append(None)
+
+        if dest in airport_size_df.columns:
+            ap_size['dest'].append(airport_size_df[dest].iloc[0] + airport_size_df[dest].iloc[1])
+        else:
+            ap_size['dest'].append(None)
+
+        # Get distance and direction
+        latlon0 = [getattr(row, 'Origin_Lat'), getattr(row, 'Origin_Lon')]
+        latlon1 = [getattr(row, 'Dest_Lat'), getattr(row, 'Dest_Lon')]
+        a, d = cu.get_direction_range(latlon0, latlon1)
+        dist.append(d)
+        dirctn.append(a)
+    sub_df['StraightLineDistance_Miles'] = dist
+    sub_df['Direction_Deg'] = dirctn
+    sub_df['OriginHubStatus'] = hub_stat['orgn']
+    sub_df['DestHubStatus'] = hub_stat['dest']
+    sub_df['OriginTotalFlights'] = ap_size['orgn']
+    sub_df['DestTotalFlights'] = ap_size['dest']
+
+    return sub_df
 
 
 def extract_all_files(zip_dir, base_dir, prime_airport=None):
@@ -78,14 +144,14 @@ def output_cleaned_data_to_structure(df, base_dir):
     month = df['Month'].iloc[0]
     if len(str(month)) == 1:
         month = '0{}'.format(month)
-    filename = '{}{}_OnTimeData.parquet'.format(year, month)
+    filename = '{}{}_OnTimeData.csv'.format(year, month)
 
     dataset_dir = Path(base_dir, 'dataset')
     if not dataset_dir.exists():
         dataset_dir.mkdir()
 
     output_path = Path(dataset_dir, filename)
-    df.to_parquet(output_path, index=False)
+    df.to_csv(output_path, index=False)
 
 
 def extract_parquet_to_csv(filepath):
@@ -95,10 +161,14 @@ def extract_parquet_to_csv(filepath):
 
 
 if __name__ == '__main__':
-    _BASE_DIR = r'C:\Users\pfarmer8-gtri\Box\Paul_Farmer\Grad School\Material\CSE6242 - Data and Visual Analytics\Project'
+    _BASE_DIR = r'C:\Users\Paul\OneDrive - Georgia Institute of Technology\Grad_School\Courses\CSE6242\Projects\dataset'
     _PRIME_AIRPORT = 'SAN'  # San Diego
     extract_all_files(_ZIP_DATA_DIR_, _BASE_DIR, _PRIME_AIRPORT)
 
-    # NOTE: Delete extracted CSV files
-    DF = extract_parquet_to_csv(Path(_BASE_DIR, 'dataset', '201801_OnTimeData.parquet'))
-    DF.to_csv(str(Path(_BASE_DIR, 'test_csv_201801.csv')))
+    EXTRACT_DIR = Path(_ZIP_DATA_DIR_, 'temp_extracted')
+    for file in EXTRACT_DIR.iterdir():
+        file.unlink(missing_ok=True)
+    EXTRACT_DIR.rmdir()
+
+    # DF = extract_parquet_to_csv(Path(_BASE_DIR, 'dataset', '201801_OnTimeData.parquet'))
+    # DF.to_csv(str(Path(_BASE_DIR, 'test_csv_201801.csv')))

ebb438c2f554dc9fb1cf4b06a0798d0bfd62ee83
stringofQs <100437339+stringofQs@users.noreply.github.com>
2023-04-16 15:40:37 -0400

    Updates to Data Cleaning
    Improvements and updates to data cleaning scripts to make it more "pipeline"-like. Should enable a user to download new datasets from the data source, run it through this cleaner, then run it through the modeler to get new predictions on a new dataset

--- dataManagement/data_cleaner.py
+++ dataManagement/data_cleaner.py
@@ -13,7 +13,24 @@ _COLS_OF_INTEREST = ['Year', 'Quarter', 'Month', 'DayofMonth', 'DayOfWeek',
                      'Origin', 'OriginCityName', 'OriginState', 'DestAirportID', 'DestCityMarketID',
                      'Dest', 'DestCityName', 'DestState', 'CRSDepTime', 'DepTime', 'DepTimeBlk',
                      'CRSArrTime', 'ArrTime', 'ArrTimeBlk', 'Cancelled', 'CancellationCode',
-                     'Distance', 'DistanceGroup', 'CarrierDelay', 'NASDelay', 'LateAircraftDelay']
+                     'Distance', 'DistanceGroup', 'CarrierDelay', 'WeatherDelay', 'NASDelay', 'LateAircraftDelay']
+
+_REGION_MAP = {'AK': 'Alaska', 'AL': 'EastSouthCentral', 'AR': 'WestSouthCentral', 'AZ': 'MountainSouth',
+               'CA': 'Pacific', 'CO': 'MountainSouth', 'CT': 'NewEngland', 'DE': 'MidAtlantic', 'DC': 'MidAtlantic',
+               'FL': 'SouthAtlantic', 'GA': 'SouthAtlantic', 'HI': 'Hawaii', 'ID': 'MountainNorth',
+               'IN': 'MidwestGreatLakes', 'IA': 'MidwestCentral', 'IL': 'MidwestGreatLakes', 'KS': 'MidwestCentral',
+               'KY': 'EastSouthCentral', 'LA': 'WestSouthCentral', 'ME': 'NewEngland', 'MD': 'MidAtlantic',
+               'MA': 'NewEngland', 'MI': 'MidwestGreatLakes', 'MN': 'MidwestCentral', 'MS': 'EastSouthCentral',
+               'MO': 'MidwestCentral', 'MT': 'MountainNorth', 'NE': 'MidwestCentral', 'NV': 'MountainSouth',
+               'NH': 'NewEngland', 'NJ': 'MidAtlantic', 'NM': 'MountainSouth', 'NY': 'MidAtlantic',
+               'NC': 'SouthAtlantic', 'ND': 'MidwestCentral', 'OH': 'MidwestGreatLakes', 'OK': 'WestSouthCentral',
+               'OR': 'Pacific', 'PA': 'MidAtlantic', 'PR': 'Caribbean', 'RI': 'NewEngland', 'SC': 'SouthAtlantic',
+               'SD': 'MidwestCentral', 'TN': 'EastSouthCentral', 'TX': 'WestSouthCentral', 'UT': 'MountainSouth',
+               'VA': 'SouthAtlantic', 'VT': 'NewEngland', 'VI': 'Caribbean', 'WA': 'Pacific', 'WV': 'SouthAtlantic',
+               'WI': 'MidwestGreatLakes', 'WY': 'MountainNorth'}
+_REGION_CAT_MAP = {'Alaska': 1, 'Hawaii': 2, 'Pacific': 3, 'MountainSouth': 4, 'MountainNorth': 5, 'MidwestCentral': 6,
+                   'WestSouthCentral': 7, 'EastSouthCentral': 8, 'MidwestGreatLakes': 9, 'SouthAtlantic': 10,
+                   'MidAtlantic': 11, 'NewEngland': 12, 'Caribbean': 13}
 
 
 def extract_csv_from_zip(zip_file_path):
@@ -49,19 +66,48 @@ def clean_dataframe(df, prime_airport=None):
         if prime_airport is not None:
             print('Could not find the defined airport in the dataset: {}'.format(prime_airport))
         final_df = df.copy()
-    all_unq_airports = list(set(list(df['Origin'].unique()) + list(df['Dest'].unique())))
-    busy_count = {'Airport': ['Dep', 'Arr']}
-    for apt in all_unq_airports:
-        if apt not in busy_count:
-            busy_count[apt] = [0, 0]
-        busy_count[apt][0] = (df.Origin.values == apt).sum()
-        busy_count[apt][1] = (df.Dest.values == apt).sum()
+    if not Path('supplemental_data', 'airport_size.csv').exists():
+        all_unq_airports = list(set(list(df['Origin'].unique()) + list(df['Dest'].unique())))
+        busy_count = {'Airport': ['Dep', 'Arr']}
+        for apt in all_unq_airports:
+            if apt not in busy_count:
+                busy_count[apt] = [0, 0]
+            busy_count[apt][0] = (df.Origin.values == apt).sum()
+            busy_count[apt][1] = (df.Dest.values == apt).sum()
 
-    airport_size_df = pd.DataFrame(busy_count).reset_index(drop=True)
-    airport_size_df.to_csv('supplemental_data/airport_size.csv')
+        airport_size_df = pd.DataFrame(busy_count).reset_index(drop=True)
+        airport_size_df.to_csv(str(Path('supplemental_data', 'airport_size.csv')))
+    else:
+        airport_size_df = pd.read_csv(str(Path('supplemental_data', 'airport_size.csv')))
+
     fixed_df = finalize_dataset(final_df, airport_size_df)
 
     return fixed_df
+
+
+def get_hub_status(airport, carrier, hub_df):
+    if carrier not in hub_df.columns:
+        return 0
+    else:
+        subset = hub_df[(hub_df['Airport'] == airport)]
+        if not subset.empty:
+            return subset[carrier].iloc[0]
+        else:
+            return 0
+
+
+def get_airport_size(airport, size_df):
+    if airport in size_df.columns:
+        return size_df[airport].iloc[0] + size_df[airport].iloc[1]
+    else:
+        return None
+
+
+def get_region_category(state):
+    if state in _REGION_MAP.keys():
+        return _REGION_CAT_MAP[_REGION_MAP[state]]
+    else:
+        return None
 
 
 def finalize_dataset(df, airport_size_df):
@@ -82,47 +128,14 @@ def finalize_dataset(df, airport_size_df):
     unnamed_cols = [i for i in sub_df.columns if 'unnamed' in i.lower()]
     sub_df.drop(unnamed_cols, inplace=True, axis=1)
 
-    dist = []
-    dirctn = []
-    ap_size = {'orgn': [], 'dest': []}
-    hub_stat = {'orgn': [], 'dest': []}
-    for ndx, row in sub_df.iterrows():
-        # Get hub status
-        orgn = getattr(row, 'Origin')
-        dest = getattr(row, 'Dest')
-        carrier = getattr(row, 'Marketing_Airline_Network')
-        if (carrier in hubs.columns) and (orgn in list(hubs['Airport'].unique())):
-            hub_stat['orgn'].append(hubs[hubs['Airport'] == orgn][carrier].iloc[0])
-        else:
-            hub_stat['orgn'].append(0)  # If not present, is not a hub
-
-        if (carrier in hubs.columns) and (dest in list(hubs['Airport'].unique())):
-            hub_stat['dest'].append(hubs[hubs['Airport'] == dest][carrier].iloc[0])
-        else:
-            hub_stat['dest'].append(0)  # If not present, is not a hub
-
-        if orgn in airport_size_df.columns:
-            ap_size['orgn'].append(airport_size_df[orgn].iloc[0] + airport_size_df[orgn].iloc[1])
-        else:
-            ap_size['orgn'].append(None)
-
-        if dest in airport_size_df.columns:
-            ap_size['dest'].append(airport_size_df[dest].iloc[0] + airport_size_df[dest].iloc[1])
-        else:
-            ap_size['dest'].append(None)
-
-        # Get distance and direction
-        latlon0 = [getattr(row, 'Origin_Lat'), getattr(row, 'Origin_Lon')]
-        latlon1 = [getattr(row, 'Dest_Lat'), getattr(row, 'Dest_Lon')]
-        a, d = cu.get_direction_range(latlon0, latlon1)
-        dist.append(d)
-        dirctn.append(a)
-    sub_df['StraightLineDistance_Miles'] = dist
-    sub_df['Direction_Deg'] = dirctn
-    sub_df['OriginHubStatus'] = hub_stat['orgn']
-    sub_df['DestHubStatus'] = hub_stat['dest']
-    sub_df['OriginTotalFlights'] = ap_size['orgn']
-    sub_df['DestTotalFlights'] = ap_size['dest']
+    sub_df[['StraightLineDistance_Miles', 'Direction_Deg']] = sub_df.apply(
+        lambda x: cu.get_direction_range([x['Origin_Lat'], x['Origin_Lon']], [x['Dest_Lat'], x['Dest_Lon']]), axis=1)
+    sub_df['OriginTotalFlights'] = sub_df.apply(lambda x: get_airport_size(x['Origin'], airport_size_df), axis=1)
+    sub_df['DestTotalFlights'] = sub_df.apply(lambda x: get_airport_size(x['Dest'], airport_size_df), axis=1)
+    sub_df['OriginGeographicCentrality'] = sub_df.apply(
+        lambda x: cu.get_geographic_centrality_category(x['Origin_Lat'], x['Origin_Lon']), axis=1)
+    sub_df['DestGeographicCentrality'] = sub_df.apply(
+        lambda x: cu.get_geographic_centrality_category(x['Dest_Lat'], x['Dest_Lon']), axis=1)
 
     return sub_df
 
@@ -131,8 +144,11 @@ def extract_all_files(zip_dir, base_dir, prime_airport=None):
     if Path(zip_dir).is_dir():
         for fle in Path(zip_dir).iterdir():
             if fle.suffix == '.zip':
+                print('\nProcessing file: {}\n'.format(fle))
                 df = extract_csv_from_zip(fle)
+                print('\n\tCleaning...')
                 cleaned_df = clean_dataframe(df, prime_airport=prime_airport)
+                print('\n\tOutputting...')
                 output_cleaned_data_to_structure(cleaned_df, base_dir)
 
 
@@ -161,14 +177,46 @@ def extract_parquet_to_csv(filepath):
 
 
 if __name__ == '__main__':
-    _BASE_DIR = r'C:\Users\Paul\OneDrive - Georgia Institute of Technology\Grad_School\Courses\CSE6242\Projects\dataset'
-    _PRIME_AIRPORT = 'SAN'  # San Diego
+    # UNCOMMENT TO RUN ----------------------------------------------
+    _BASE_DIR = input('Input base directory that the downloaded dataset exists in: ')
+    # _BASE_DIR = r'C:\Users\Paul\OneDrive - Georgia Institute of Technology\Grad_School\Courses\CSE6242\Projects\dataset'
+    _PRIME_AIRPORT = input('Input the primary airport of interest for the modeling and visualization. '
+                           'This will be the origin airport: ')
+    # _PRIME_AIRPORT = 'SAN'  # San Diego
     extract_all_files(_ZIP_DATA_DIR_, _BASE_DIR, _PRIME_AIRPORT)
 
     EXTRACT_DIR = Path(_ZIP_DATA_DIR_, 'temp_extracted')
     for file in EXTRACT_DIR.iterdir():
         file.unlink(missing_ok=True)
     EXTRACT_DIR.rmdir()
+    # ---------------------------------------------------------------
 
     # DF = extract_parquet_to_csv(Path(_BASE_DIR, 'dataset', '201801_OnTimeData.parquet'))
     # DF.to_csv(str(Path(_BASE_DIR, 'test_csv_201801.csv')))
+
+    # from plotly import graph_objects as go
+    # color_scheme = ['#9e0142', '#d53e4f', '#f46d43', '#fdae61', '#fee08b',
+    #                 '#e6f598', '#abdda4', '#66c2a5', '#3288bd', '#5e4fa2']
+    #
+    # df = pd.read_excel(r'C:\Users\Paul\OneDrive - Georgia Institute of Technology\Grad_School\Courses\CSE6242\Projects\repository\gatech-cse6242-spring2023-project\dataManagement\supplemental_data\Airport_locations_v2.xlsx')
+    # x = []
+    # y = []
+    # catList = []
+    # namelist = []
+    # for ndx, row in df.iterrows():
+    #     lat = getattr(row, 'FAA_Lat')
+    #     lon = getattr(row, 'FAA_Long')
+    #     name = getattr(row, 'Airport')
+    #     state = getattr(row, 'Airport_State')
+    #
+    #     cat = get_region_category(state)
+    #     if cat is None:
+    #         continue
+    #     x.append(lon)
+    #     y.append(lat)
+    #     catList.append(color_scheme[(cat-1)%10])
+    #     namelist.append(name)
+    #
+    # data = [go.Scatter(x=x, y=y, mode='markers', marker=dict(color=catList))]
+    # fig = go.Figure(data=data)
+    # fig.show()
